230962824bc1223fd736b4e9f35f93db9b9744a6
Gavin <gavinwan0929@gmail.com>
2023-04-21 10:46:09 -0400

    Project3 up6

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import sqlite3
 application = Flask(__name__)
 app = application
 
-application.config['SECRET_KEY'] = 'your secret key'
+# application.config['SECRET_KEY'] = 'your secret key'
 
 def get_db_connection():
     conn = sqlite3.connect('tornadograph.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-@application.route('/create/', methods=('GET', 'POST'))
-def create():
-    return render_template('create.html')
+# @application.route('/create/', methods=('GET', 'POST'))
+# def create():
+#     return render_template('create.html')
 
 @application.route('/')
 def index():
